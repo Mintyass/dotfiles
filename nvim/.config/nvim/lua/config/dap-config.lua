b7b20cc2ea6c33c6336b35526b5a0dd8fdb9eb1b
Chris Mintzias <chris.mintzias@gmail.com>
2024-06-03 06:04:38 -0700

    copilot

--- nvim/.config/nvim/lua/config/dap-config.lua
+++ nvim/.config/nvim/lua/config/dap-config.lua
@@ -20,10 +20,17 @@ dap.configurations.cpp = {
     type = "codelldb",
     request = "launch",
     program = function()
-      return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+      return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/bazel-bin/NESMint/Main", "file")
     end,
     cwd = "${workspaceFolder}",
     stopOnEntry = false,
+    setupCommands = {
+      {
+        text = '-enable-pretty-printing',
+        description = 'enable pretty printing',
+        ignoreFailures = false
+      },
+    },
     args = {},
 
     -- Source mapping to resolve breakpoints correctly
@@ -33,7 +40,37 @@ dap.configurations.cpp = {
 
     runInTerminal = false,
   },
+  {
+    name = "Launch NESMintApp",
+    type = "codelldb",
+    request = "launch",
+    program = vim.fn.getcwd() .. "/bazel-bin/NESMintApp/NESMintApp",
+    cwd = "${workspaceFolder}",
+    stopOnEntry = false,
+
+    setupCommands = {
+      {
+        text = '-enable-pretty-printing',
+        description = 'enable pretty printing',
+        ignoreFailures = false
+      },
+    },
+    args = {},
+
+    -- Source mapping to resolve breakpoints correctly
+    sourceMap = {
+      ["/proc/self/cwd"] = vim.fn.getcwd(),
+    },
+
+    runInTerminal = false,
+  }
 }
+
+-- Key mappings
+vim.api.nvim_set_keymap('n', '<F5>', '<Cmd>lua require"dap".continue()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<F10>', '<Cmd>lua require"dap".step_over()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<F11>', '<Cmd>lua require"dap".step_into()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<F12>', '<Cmd>lua require"dap".step_out()<CR>', { noremap = true, silent = true })
 
 -- Apply the same configuration to C files
 dap.configurations.c = dap.configurations.cpp
--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+
+-- Key mappings
+vim.api.nvim_set_keymap('n', '<F5>', '<Cmd>lua require"dap".continue()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<F10>', '<Cmd>lua require"dap".step_over()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<F11>', '<Cmd>lua require"dap".step_into()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<F12>', '<Cmd>lua require"dap".step_out()<CR>', { noremap = true, silent = true })
